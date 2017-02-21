d54193b98d0ff1ab6d75e6cc36032fc33b0ec1d0
LMH <ymw0608@naver.com>
2017-02-21 14:08:26 +0900

    [임문환] LoginVo 변경

---
+++ SingleTonTrip/src/main/java/com/log4h/singletontrip/member/domain/LoginVo.java
@@ -0,0 +1,26 @@
+package com.log4h.singletontrip.member.domain;
+
+public class LoginVo {
+	private String memberId;
+	private int memberLevel;
+	private String memberNm;
+	
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public int getMemberLevel() {
+		return memberLevel;
+	}
+	public void setMemberLevel(int memberLevel) {
+		this.memberLevel = memberLevel;
+	}
+	public String getMemberNm() {
+		return memberNm;
+	}
+	public void setMemberNm(String memberNm) {
+		this.memberNm = memberNm;
+	}
+}
