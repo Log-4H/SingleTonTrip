8c6db0d4ab23a82b526db231b32761e453e8b5e6
dmlgusthd@naver.com <dmlgusthd@naver.com>
2017-02-22 17:25:57 +0900

    [김의현] 광고신청,신청리스트 까지 	신청 등록하고 payment로 이동하는건 아직안함

---
+++ SingleTonTrip/src/main/java/com/log4h/singletontrip/ad/domain/AdVo.java
@@ -0,0 +1,98 @@
+package com.log4h.singletontrip.ad.domain;
+
+public class AdVo {
+	private int adNo;			// 광고no
+	private String companyId;	// 업체 아이디
+	private int adPriceCd;		// 광고 단가 code
+	private int approveStateCd;	// 승인 상태 code
+	private String adContent;	// 광고 내용
+	private String adImg;		// 광고 이미지
+	private int adTotalAmount;	// 총수량(일수)
+	private int adTotalPrice;	// 총결제금액
+	private String adApplyDate;	// 광고 신청일
+	private String adRegDate;	// 광고 게시일
+	private String adEndDate;	// 광고 종료일
+	private String adminId;		// 관리자 아이디
+	
+	public int getAdNo() {
+		return adNo;
+	}
+	public void setAdNo(int adNo) {
+		this.adNo = adNo;
+	}
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public int getAdPriceCd() {
+		return adPriceCd;
+	}
+	public void setAdPriceCd(int adPriceCd) {
+		this.adPriceCd = adPriceCd;
+	}
+	public int getApproveStateCd() {
+		return approveStateCd;
+	}
+	public void setApproveStateCd(int approveStateCd) {
+		this.approveStateCd = approveStateCd;
+	}
+	public String getAdContent() {
+		return adContent;
+	}
+	public void setAdContent(String adContent) {
+		this.adContent = adContent;
+	}
+	public String getAdImg() {
+		return adImg;
+	}
+	public void setAdImg(String adImg) {
+		this.adImg = adImg;
+	}
+	public int getAdTotalAmount() {
+		return adTotalAmount;
+	}
+	public void setAdTotalAmount(int adTotalAmount) {
+		this.adTotalAmount = adTotalAmount;
+	}
+	public int getAdTotalPrice() {
+		return adTotalPrice;
+	}
+	public void setAdTotalPrice(int adTotalPrice) {
+		this.adTotalPrice = adTotalPrice;
+	}
+	public String getAdApplyDate() {
+		return adApplyDate;
+	}
+	public void setAdApplyDate(String adApplyDate) {
+		this.adApplyDate = adApplyDate;
+	}
+	public String getAdRegDate() {
+		return adRegDate;
+	}
+	public void setAdRegDate(String adRegDate) {
+		this.adRegDate = adRegDate;
+	}
+	public String getAdEndDate() {
+		return adEndDate;
+	}
+	public void setAdEndDate(String adEndDate) {
+		this.adEndDate = adEndDate;
+	}
+	public String getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	
+	@Override
+	public String toString() {
+		return "Ad [adNo=" + adNo + ", companyId=" + companyId + ", adPriceCd=" + adPriceCd + ", approveStateCd="
+				+ approveStateCd + ", adContent=" + adContent + ", adImg=" + adImg + ", adTotalAmount=" + adTotalAmount
+				+ ", adTotalPrice=" + adTotalPrice + ", adApplyDate=" + adApplyDate + ", adRegDate=" + adRegDate
+				+ ", adEndDate=" + adEndDate + ", adminId=" + adminId + "]";
+	}
+	
+}
--- SingleTonTrip/src/main/java/com/log4h/singletontrip/ad/service/AdService.java
+++ SingleTonTrip/src/main/java/com/log4h/singletontrip/ad/service/AdService.java
@@ -1,5 +1,13 @@
 package com.log4h.singletontrip.ad.service;
 
-public interface AdService {
+import java.util.List;
 
+import com.log4h.singletontrip.ad.domain.AdPriceVo;
+import com.log4h.singletontrip.ad.domain.AdVo;
+
+
+public interface AdService {
+	public List<AdPriceVo> selectAdPrice();
+	public int adApply(AdVo adVo);
+	public List<AdVo> selectAdApplyList();
 }
