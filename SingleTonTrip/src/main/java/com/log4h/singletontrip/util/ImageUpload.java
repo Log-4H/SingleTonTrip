d4fae57ce54ba0c8b788bd40661a67159d15793a
LMH <ymw0608@naver.com>
2017-03-02 14:24:58 +0900

    [임문환] 이미지업로드 클래스 수정

--- SingleTonTrip/src/main/java/com/log4h/singletontrip/util/ImageUpload.java
+++ SingleTonTrip/src/main/java/com/log4h/singletontrip/util/ImageUpload.java
@@ -3,37 +3,46 @@ package com.log4h.singletontrip.util;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
+
 import org.springframework.web.multipart.MultipartFile;
 
-public class ImageUpload {
-
-	public String uploadImage(MultipartFile imgFile) {
-		// cafe24 절대 경로 /home/hosting_users/본인ID/www/디렉토리
-		String realPath = "D:\\sts\\SingleTonTrip\\SingleTonTrip\\src\\main\\webapp\\images\\";
+public class ImageUpload {	
+	//cafe24 절대 경로  /home/hosting_users/본인ID/www/디렉토리
+	private final String realPath = "D:\\TeamProject\\git\\SingleTonTrip\\SingleTonTrip\\src\\main\\webapp\\images\\";
+	
+	//업로드 이미지
+	public String uploadImage(MultipartFile imgFile){
 		String uploadPath = null;
 		String imgName = null;
 		String extension = null;
 		File destFile = null;
+
 		try {
 			imgName = imgFile.getOriginalFilename();
-			if (!imgName.equals("")) {
+			if(!imgName.equals("")){
 				int index = imgName.lastIndexOf(".");
-				extension = "." + imgName.substring(index + 1);
+				extension = "."+imgName.substring(index+1);
 				UUID uuid = UUID.randomUUID();
-
+				
 				imgName = uuid.toString().replace("-", "") + extension;
 				uploadPath = realPath + imgName;
 				destFile = new File(uploadPath);
-
+				
 				imgFile.transferTo(destFile);
 			}
 		} catch (IllegalStateException e) {
-			destFile.delete();
+			destFile.delete(); 
 			e.printStackTrace();
 		} catch (IOException e) {
 			destFile.delete();
 			e.printStackTrace();
 		}
-		return imgName;
+		return imgName;	
+	}
+	
+	//업로드 파일 삭제
+	public void deleteImage(String postImg){		
+		File destFile = new File(realPath + postImg);
+		destFile.delete();
 	}
 }
